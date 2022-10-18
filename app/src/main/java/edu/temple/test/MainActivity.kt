50c93091773f4e46d9578e101da11c7b34649b39
karenkraft1130 <tuk04262@temple.edu>
2022-10-18 11:37:47 -0400

    added textViewModel

--- app/src/main/java/edu/temple/test/MainActivity.kt
+++ app/src/main/java/edu/temple/test/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 import org.w3c.dom.Text
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity(), SizeFragment.ValueChangeInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
         // Create and attach fragments if not already exist
---
+++ app/src/main/java/edu/temple/test/textViewModel.kt
@@ -0,0 +1,20 @@
+package edu.temple.test
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class textViewModel : ViewModel() {
+
+    private val textSize : MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+
+    fun getColor () : LiveData<Int> {
+        return textSize
+    }
+
+    fun setColor (text: Int) {
+        textSize.value = text
+    }
+}
